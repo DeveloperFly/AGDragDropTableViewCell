6fc4ad039ed6caafc023f795ee4dc0600bff878a
Aman <aman19ish@gmail.com>
2017-12-29 10:32:31 +0530

    Create TableViews for move TableViewCell

---
+++ AGDragDropTableViewCell/Cells/TopTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TopTableViewCell.swift
+//  AGDragDropTableViewCell
+//
+//  Created by Aman Gupta on 29/12/17.
+//  Copyright © 2017 Developer Fly. All rights reserved.
+//
+
+import UIKit
+
+class TopTableViewCell: UITableViewCell {
+    //MARK: - IBOutlets
+    @IBOutlet weak var labelAge: UILabel!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var imageViewProfile: UIImageView!
+    
+    //MARK: - Configure Cell Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- AGDragDropTableViewCell/ViewController.swift
+++ AGDragDropTableViewCell/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var bottomTableView: UITableView!
+    @IBOutlet weak var topTableView: UITableView!
+    //MARK: - View Lide Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == topTableView {
+            let cell = topTableView.dequeueReusableCell(withIdentifier: "TopTableViewCell", for: indexPath) as! TopTableViewCell
+            return cell
+        } else {
+            let cell = bottomTableView.dequeueReusableCell(withIdentifier: "BottomTableViewCell", for: indexPath) as! BottomTableViewCell
+            return cell
+        }
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
 
